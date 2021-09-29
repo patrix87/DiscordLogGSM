ac5cc953d6ce044ff1e082b80d18e40c8b4c604a
Patrick Veilleux <patrix87@gmail.com>
2021-09-28 23:27:41 -0400

    fix players

--- bot.py
+++ bot.py
@@ -232,8 +232,11 @@ class DiscordGSM():
 
         players = self.get_value(data, "players", "?")
         bots = self.get_value(data, "bots")
-        if cache_status != "Online": 
+        if cache_status == "Offline": 
             players = 0
+            bots = None
+        if data is False: 
+            players = "?"
             bots = None
         maxplayers = self.get_value(data, "maxplayers") or self.get_value(server, "maxplayers") or "?"
         players_string = f'{players}({bots})/{maxplayers}' if bots is not None and bots > 0 else f'{players}/{maxplayers}'
